b165913282cfd594d5e000b138b53cbe7b4a8cbc
kyeong-hyeok <minkh4227@naver.com>
2023-05-26 14:32:41 +0900

    [MOD] AppConfig 리팩토링

--- src/main/java/spring/core/AppConfig.java
+++ src/main/java/spring/core/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.core;
 
+import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
 import spring.core.member.MemoryMemberRepository;
@@ -8,14 +10,21 @@ import spring.core.order.OrderService;
 import spring.core.order.OrderServiceImpl;
 
 public class AppConfig {
-
+    // 역할과 구현 클래스가 한눈에 들어옴 -> 애플리케이션 전체 구성 빠르게 파악 가능
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 }
