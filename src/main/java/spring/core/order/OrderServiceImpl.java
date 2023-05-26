423ebe22a986649accbba96fcab7ed68ecd93864
kyeong-hyeok <minkh4227@naver.com>
2023-05-26 11:07:25 +0900

    [ADD] 정률 할인 정책 코드 추가

--- src/main/java/spring/core/order/OrderServiceImpl.java
+++ src/main/java/spring/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package spring.core.order;
 
 import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.discount.RateDiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
 import spring.core.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import spring.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
