39da9bd62d1839415d01de248f6fd6a71f44fbb9
kyeong-hyeok <minkh4227@naver.com>
2023-06-22 21:23:52 +0900

    [ADD] Provider을 이용한 request scope 빈의 생성 지연

--- src/main/java/spring/core/web/LogDemoController.java
+++ src/main/java/spring/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package spring.core.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,13 @@ import spring.core.common.MyLogger;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
